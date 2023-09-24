8d1c94a6bea1b63fdca43cb4c7d36cbf9f8aafde
SurajSingh7 <220suraj@gmail.com>
2023-09-24 23:54:34 +0530

    All moviesList

---
+++ src/utils/constants.js
@@ -0,0 +1,411 @@
+export const TypeMovieObj={
+    1:"BOLLYWOOD MOVIES",
+    2:"SOUTH HINDI DUBBED",
+    3:"POPULAR MOVIES",
+    4:"WEB SERIES",
+    5:"OLD IS GOLD MOVIES",
+    6:"HOLLYWOOD(HINDI)"
+  };
+  
+  // id,name,image,url,movieType
+  export const moviesList=[
+   {
+      
+        id: "0",
+        name: "0 Kaithi (4K) - कार्थी की ब्लॉकबस्टर एक्शन थ्रिलर हिंदी मूवी| Narain, Arjun Das, George Maryan, Harish",
+        image: "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/c698c4a3574f7206f444ea6aa450c702abe1a55551a583f1f985dc47b64c97d1._RI_TTW_.jpg",
+        url:  "https://youtu.be/-I12_W7UHh0",
+        movieType:"23",
+        showRating:"5"
+  
+       
+   },
+   {
+    
+      id: "1",
+      name: "1 Aravind Sametha (4K) | Jr NTR, Pooja Hegde, Jagapathi Babu, Eesha Rebba | New Hindi Dubbed Movie",
+      image: "https://static.moviecrow.com/gallery/20180521/139559-Aravinda%20Sametha%20Veera%20Raghava.jpg",
+      url:"https://youtu.be/4jtbe0I1e1c",
+      movieType:"2",
+      showRating:"5"
+    },
+   {
+      id: "2",
+      name: "2 K.G.F Full Movie | Yash, Srinidhi Shetty, Ananth Nag, Ramachandra Raju, Achyuth Kumar, Malavika",
+      image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR-GJWOluiuP4BTtHHg6Kk68cuueul3mwQOMwueqol3kBMrT8c7",
+      url:"https://youtu.be/ULEQb_l-N08",
+      movieType:"23",
+      showRating:"5"
+  
+   },
+   {
+  
+      id: "3",
+      name: "3 Bermuda Tentacles | Hindi dubbed Hollywood full movie | Hollywood Movie Dubbed in Hindi",
+      image: "https://flxt.tmsimg.com/assets/p10648316_p_v10_aa.jpg",
+      url:"https://youtu.be/JtmCRZqWW5o",
+      movieType:"6",
+      showRating:"5"
+   },
+   {
+    
+      id: "4",
+      name: "4 Bhool Bhulaiyaa (Full Movie) Akshay Kumar, Vidya Balan, Shiney A, Paresh R, Priyadarshan | Bhushan K",
+      image: "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/bhool-bhulaiyaa-et00000097-24-03-2017-16-39-50.jpg",
+      url:"https://youtu.be/jzYxbnHHhY4",
+      movieType:"15",
+      showRating:"5"
+  
+    
+  
+   },
+   {
+   
+      id: "5",
+      name: "5 Magadheera (4K Ultra HD) Hindi Dubbed Movie | Ram Charan, Kajal Aggarwal, Dev Gill, Srihari",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRniE6jjn-VfygT4MUldYmnxSOfErBq65Np9Q&usqp=CAU",
+      url:"https://youtu.be/n9UxHQ1unn8",
+      movieType:"23",
+      showRating:"5"
+    
+  
+   },
+   {
+      id: "6",
+      name: "6 Asur: Welcome to Your Dark Side (TV Series 2020– ) Asur: Welcome to Your Dark Side (TV Series 2020– ) ",
+      image: "https://m.media-amazon.com/images/M/MV5BNjczODVjMmMtNTVlNy00MjlkLWEyZjYtOThiYzMwZmIyNWZkXkEyXkFqcGdeQXVyMTY0NjI3Mjcx._V1_.jpg",
+      url:"https://youtu.be/11OWuPcElJw",
+      movieType:"4",
+      showRating:"5"
+   },
+   {
+      
+     id: "7",
+     name: "7 pnik(4K ULTRA HD) Full Hindi Dubbed Movie | Allu Arjun, Rakul Preet Singh, Catherine Tresa",
+     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK3rMf7hP-awenUzC0tTfUOL436blrYFjKCQ&usqp=CAU",
+     url:  "https://youtu.be/xZ1FXdIGf9Q",
+     movieType:"2",
+     showRating:"5"
+  
+    
+  },
+  {
+  
+   id: "8",
+   name: "8Asur: Welcome to Your Dark Side (TV Series 2020– ) Asur: Welcome to Your Dark Side (TV Series 2020– ) ",
+   image: "https://m.media-amazon.com/images/M/MV5BNjczODVjMmMtNTVlNy00MjlkLWEyZjYtOThiYzMwZmIyNWZkXkEyXkFqcGdeQXVyMTY0NjI3Mjcx._V1_.jpg",
+   url:"https://youtu.be/JXvMgM5NfXQ",
+   movieType:"4",
+   showRating:"5"
+  },
+  {
+   id: "9",
+   name: "9 K.G.F: Chapter 2 was theatrically released in India on 14 April 2022 in Kannada, along with dubbed versions in Hindi",
+   image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR-GJWOluiuP4BTtHHg6Kk68cuueul3mwQOMwueqol3kBMrT8c7",
+   url:"https://youtu.be/bUR_FKt7Iso",
+   movieType:"2",
+   showRating:"5"
+  
+  },
+  {
+  
+   id: "10",
+   name: "10Baahubali 2: The Conclusion (2017) Baahubali 2: The Conclusion (2017) Baahubali 2: The Conclusion (2017)",
+   image: "https://m.media-amazon.com/images/M/MV5BMmM3ODk3MDgtZjQwZi00Y2ZhLTg0YTItMTJiNmNjNzRkYmQ4XkEyXkFqcGdeQXVyNzU3Nzk4MDQ@._V1_QL75_UX190_CR0,2,190,281_.jpg",
+   url:"https://youtu.be/11OWuPcElJw",
+   movieType:"32",
+   showRating:"5"
+  },
+  {
+  
+   id: "11",
+   name: "11Pushpa: The Rule - Part 2 (2023)Pushpa: The Rule - Part 2 (2023)",
+   image: "https://m.media-amazon.com/images/M/MV5BNGZlNTFlOWMtMzUwNC00ZDdhLTk0MWUtOGZjYzFlOTBmNDdhXkEyXkFqcGdeQXVyMTUyNjIwMDEw._V1_.jpg",
+   url:"https://youtu.be/11OWuPcElJw",
+   movieType:"2",
+   showRating:"5"
+  
+  
+  
+  },
+  {
+  
+   id: "12",
+   name: "12Panchayat (TV Series 2020– ) -Panchayat (TV Series 2020– ) -Panchayat (TV Series 2020– ) -",
+   image: "https://m.media-amazon.com/images/M/MV5BMTllYzQ0MzctZTI4Ny00YTE2LTkzYjktMTc4ZWE2MDUxNzk2XkEyXkFqcGdeQXVyNzEyNTM4MTU@._V1_.jpg",
+   url:"https://youtu.be/ohmp6Q5xfXg",
+   movieType:"4",
+   showRating:"5"
+  
+  
+  },
+  {
+   id: "13",
+   name: "13Asur: Welcome to Your Dark Side (TV Series 2020– ) Asur: Welcome to Your Dark Side (TV Series 2020– ) ",
+   image: "https://m.media-amazon.com/images/M/MV5BNjczODVjMmMtNTVlNy00MjlkLWEyZjYtOThiYzMwZmIyNWZkXkEyXkFqcGdeQXVyMTY0NjI3Mjcx._V1_.jpg",
+   url:"https://youtu.be/11OWuPcElJw",
+   movieType:"4",
+   showRating:"5"
+  },
+  {
+      
+     id: "14",
+     name: "14pnik(4K ULTRA HD) Full Hindi Dubbed Movie | Allu Arjun, Rakul Preet Singh, Catherine Tresa",
+     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK3rMf7hP-awenUzC0tTfUOL436blrYFjKCQ&usqp=CAU",
+     url:  "https://youtu.be/xZ1FXdIGf9Q",
+     movieType:"2",
+     showRating:"5"
+  
+    
+  },
+  {
+  
+   id: "15",
+   name: "15Asur: Welcome to Your Dark Side (TV Series 2020– ) Asur: Welcome to Your Dark Side (TV Series 2020– ) ",
+   image: "https://m.media-amazon.com/images/M/MV5BNjczODVjMmMtNTVlNy00MjlkLWEyZjYtOThiYzMwZmIyNWZkXkEyXkFqcGdeQXVyMTY0NjI3Mjcx._V1_.jpg",
+   url:"https://youtu.be/JXvMgM5NfXQ",
+   movieType:"4",
+   showRating:"5"
+  },
+  {
+   id: "16",
+   name: "16K.G.F: Chapter 2 was theatrically released in India on 14 April 2022 in Kannada, along with dubbed versions in Hindi",
+   image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR-GJWOluiuP4BTtHHg6Kk68cuueul3mwQOMwueqol3kBMrT8c7",
+   url:"https://youtu.be/bUR_FKt7Iso",
+   movieType:"32",
+   showRating:"5"
+  
+  },
+  {
+  
+   id: "17",
+   name: "17Baahubali 2: The Conclusion (2017) Baahubali 2: The Conclusion (2017) Baahubali 2: The Conclusion (2017)",
+   image: "https://m.media-amazon.com/images/M/MV5BMmM3ODk3MDgtZjQwZi00Y2ZhLTg0YTItMTJiNmNjNzRkYmQ4XkEyXkFqcGdeQXVyNzU3Nzk4MDQ@._V1_QL75_UX190_CR0,2,190,281_.jpg",
+   url:"https://youtu.be/11OWuPcElJw",
+   movieType:"23",
+   showRating:"5"
+  },
+  {
+  
+   id: "18",
+   name: "18Pushpa: The Rule - Part 2 (2023)Pushpa: The Rule - Part 2 (2023)",
+   image: "https://m.media-amazon.com/images/M/MV5BNGZlNTFlOWMtMzUwNC00ZDdhLTk0MWUtOGZjYzFlOTBmNDdhXkEyXkFqcGdeQXVyMTUyNjIwMDEw._V1_.jpg",
+   url:"https://youtu.be/11OWuPcElJw",
+   movieType:"23",
+   showRating:"5"
+  
+  
+  
+  },
+  {
+  
+   id: "19",
+   name: "19Panchayat (TV Series 2020– ) -Panchayat (TV Series 2020– ) -Panchayat (TV Series 2020– ) -",
+   image: "https://m.media-amazon.com/images/M/MV5BMTllYzQ0MzctZTI4Ny00YTE2LTkzYjktMTc4ZWE2MDUxNzk2XkEyXkFqcGdeQXVyNzEyNTM4MTU@._V1_.jpg",
+   url:"https://youtu.be/ohmp6Q5xfXg",
+   movieType:"4",
+   showRating:"5"
+  
+  
+  },
+  {
+   id: "20",
+   name: "20Asur: Welcome to Your Dark Side (TV Series 2020– ) Asur: Welcome to Your Dark Side (TV Series 2020– ) ",
+   image: "https://m.media-amazon.com/images/M/MV5BNjczODVjMmMtNTVlNy00MjlkLWEyZjYtOThiYzMwZmIyNWZkXkEyXkFqcGdeQXVyMTY0NjI3Mjcx._V1_.jpg",
+   url:"https://youtu.be/11OWuPcElJw",
+   movieType:"4",
+   showRating:"5"
+  },
+  {
+      
+     id: "21",
+     name: "21pnik(4K ULTRA HD) Full Hindi Dubbed Movie | Allu Arjun, Rakul Preet Singh, Catherine Tresa",
+     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK3rMf7hP-awenUzC0tTfUOL436blrYFjKCQ&usqp=CAU",
+     url:  "https://youtu.be/xZ1FXdIGf9Q",
+     movieType:"2",
+     showRating:"5"
+  
+    
+  },
+  {
+  
+   id: "22",
+   name: "22Asur: Welcome to Your Dark Side (TV Series 2020– ) Asur: Welcome to Your Dark Side (TV Series 2020– ) ",
+   image: "https://m.media-amazon.com/images/M/MV5BNjczODVjMmMtNTVlNy00MjlkLWEyZjYtOThiYzMwZmIyNWZkXkEyXkFqcGdeQXVyMTY0NjI3Mjcx._V1_.jpg",
+   url:"https://youtu.be/JXvMgM5NfXQ",
+   movieType:"4",
+   showRating:"5"
+  },
+  {
+   id: "23",
+   name: "23K.G.F: Chapter 2 was theatrically released in India on 14 April 2022 in Kannada, along with dubbed versions in Hindi",
+   image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR-GJWOluiuP4BTtHHg6Kk68cuueul3mwQOMwueqol3kBMrT8c7",
+   url:"https://youtu.be/bUR_FKt7Iso",
+   movieType:"2",
+   showRating:"5"
+  
+  },
+  {
+  
+   id: "24",
+   name: "24Pushpa: The Rule - Part 2 (2023)Pushpa: The Rule - Part 2 (2023)",
+   image: "https://m.media-amazon.com/images/M/MV5BNGZlNTFlOWMtMzUwNC00ZDdhLTk0MWUtOGZjYzFlOTBmNDdhXkEyXkFqcGdeQXVyMTUyNjIwMDEw._V1_.jpg",
+   url:"https://youtu.be/11OWuPcElJw",
+   movieType:"2",
+   showRating:"5"
+  },
+  {
+  
+   id: "25",
+   name: "25Panchayat (TV Series 2020– ) -Panchayat (TV Series 2020– ) -Panchayat (TV Series 2020– ) -",
+   image: "https://m.media-amazon.com/images/M/MV5BMTllYzQ0MzctZTI4Ny00YTE2LTkzYjktMTc4ZWE2MDUxNzk2XkEyXkFqcGdeQXVyNzEyNTM4MTU@._V1_.jpg",
+   url:"https://youtu.be/ohmp6Q5xfXg",
+   movieType:"",
+   showRating:"5"
+  
+  
+  },
+  {
+   id: "26",
+   name: "26Asur: Welcome to Your Dark Side (TV Series 2020– ) Asur: Welcome to Your Dark Side (TV Series 2020– ) ",
+   image: "https://m.media-amazon.com/images/M/MV5BNjczODVjMmMtNTVlNy00MjlkLWEyZjYtOThiYzMwZmIyNWZkXkEyXkFqcGdeQXVyMTY0NjI3Mjcx._V1_.jpg",
+   url:"https://youtu.be/11OWuPcElJw",
+   movieType:"",
+   showRating:"5"
+  },
+  {
+   
+  id: "27",
+  name: "27pnik(4K ULTRA HD) Full Hindi Dubbed Movie | Allu Arjun, Rakul Preet Singh, Catherine Tresa",
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK3rMf7hP-awenUzC0tTfUOL436blrYFjKCQ&usqp=CAU",
+  url:  "https://youtu.be/xZ1FXdIGf9Q",
+  movieType:"",
+  showRating:"5"
+  
+  
+  },
+  {
+  
+  id: "28",
+  name: "28Asur: Welcome to Your Dark Side (TV Series 2020– ) Asur: Welcome to Your Dark Side (TV Series 2020– ) ",
+  image: "https://m.media-amazon.com/images/M/MV5BNjczODVjMmMtNTVlNy00MjlkLWEyZjYtOThiYzMwZmIyNWZkXkEyXkFqcGdeQXVyMTY0NjI3Mjcx._V1_.jpg",
+  url:"https://youtu.be/JXvMgM5NfXQ",
+  movieType:"",
+  showRating:"5"
+  },
+  {
+  id: "29",
+  name: "29K.G.F: Chapter 2 was theatrically released in India on 14 April 2022 in Kannada, along with dubbed versions in Hindi",
+  image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR-GJWOluiuP4BTtHHg6Kk68cuueul3mwQOMwueqol3kBMrT8c7",
+  url:"https://youtu.be/bUR_FKt7Iso",
+  movieType:"2",
+  showRating:"5"
+  
+  },
+  {
+  
+  id: "30",
+  name: "30Baahubali 2: The Conclusion (2017) Baahubali 2: The Conclusion (2017) Baahubali 2: The Conclusion (2017)",
+  image: "https://m.media-amazon.com/images/M/MV5BMmM3ODk3MDgtZjQwZi00Y2ZhLTg0YTItMTJiNmNjNzRkYmQ4XkEyXkFqcGdeQXVyNzU3Nzk4MDQ@._V1_QL75_UX190_CR0,2,190,281_.jpg",
+  url:"https://youtu.be/11OWuPcElJw",
+  movieType:"",
+  showRating:"5"
+  },
+  {
+  
+  id: "31",
+  name: "31Pushpa: The Rule - Part 2 (2023)Pushpa: The Rule - Part 2 (2023)",
+  image: "https://m.media-amazon.com/images/M/MV5BNGZlNTFlOWMtMzUwNC00ZDdhLTk0MWUtOGZjYzFlOTBmNDdhXkEyXkFqcGdeQXVyMTUyNjIwMDEw._V1_.jpg",
+  url:"https://youtu.be/11OWuPcElJw",
+  movieType:"",
+  showRating:"5"
+  
+  
+  
+  },
+  {
+  
+  id: "32",
+  name: "32Panchayat (TV Series 2020– ) -Panchayat (TV Series 2020– ) -Panchayat (TV Series 2020– ) -",
+  image: "https://m.media-amazon.com/images/M/MV5BMTllYzQ0MzctZTI4Ny00YTE2LTkzYjktMTc4ZWE2MDUxNzk2XkEyXkFqcGdeQXVyNzEyNTM4MTU@._V1_.jpg",
+  url:"https://youtu.be/ohmp6Q5xfXg",
+  movieType:"7",
+  showRating:"5"
+  
+  
+  },
+  {
+  id: "33",
+  name: "33Asur: Welcome to Your Dark Side (TV Series 2020– ) Asur: Welcome to Your Dark Side (TV Series 2020– ) ",
+  image: "https://m.media-amazon.com/images/M/MV5BNjczODVjMmMtNTVlNy00MjlkLWEyZjYtOThiYzMwZmIyNWZkXkEyXkFqcGdeQXVyMTY0NjI3Mjcx._V1_.jpg",
+  url:"https://youtu.be/11OWuPcElJw",
+  movieType:"7",
+  showRating:"5"
+  },
+  {
+   
+  id: "34",
+  name: "34pnik(4K ULTRA HD) Full Hindi Dubbed Movie | Allu Arjun, Rakul Preet Singh, Catherine Tresa",
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK3rMf7hP-awenUzC0tTfUOL436blrYFjKCQ&usqp=CAU",
+  url:  "https://youtu.be/xZ1FXdIGf9Q",
+  movieType:"7",
+  showRating:"5"
+  
+  
+  },
+  {
+  
+  id: "35",
+  name: "35Asur: Welcome to Your Dark Side (TV Series 2020– ) Asur: Welcome to Your Dark Side (TV Series 2020– ) ",
+  image: "https://m.media-amazon.com/images/M/MV5BNjczODVjMmMtNTVlNy00MjlkLWEyZjYtOThiYzMwZmIyNWZkXkEyXkFqcGdeQXVyMTY0NjI3Mjcx._V1_.jpg",
+  url:"https://youtu.be/JXvMgM5NfXQ",
+  movieType:"7",
+  showRating:"5"
+  },
+  {
+  id: "36",
+  name: "36K.G.F: Chapter 2 was theatrically released in India on 14 April 2022 in Kannada, along with dubbed versions in Hindi",
+  image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR-GJWOluiuP4BTtHHg6Kk68cuueul3mwQOMwueqol3kBMrT8c7",
+  url:"https://youtu.be/bUR_FKt7Iso",
+  movieType:"7",
+  showRating:"5"
+  
+  },
+  {
+  
+  id: "37",
+  name: "37Baahubali 2: The Conclusion (2017) Baahubali 2: The Conclusion (2017) Baahubali 2: The Conclusion (2017)",
+  image: "https://m.media-amazon.com/images/M/MV5BMmM3ODk3MDgtZjQwZi00Y2ZhLTg0YTItMTJiNmNjNzRkYmQ4XkEyXkFqcGdeQXVyNzU3Nzk4MDQ@._V1_QL75_UX190_CR0,2,190,281_.jpg",
+  url:"https://youtu.be/11OWuPcElJw",
+  movieType:"7",
+  showRating:"5"
+  },
+  {
+  
+  id: "38",
+  name: "38Pushpa: The Rule - Part 2 (2023)Pushpa: The Rule - Part 2 (2023)",
+  image: "https://m.media-amazon.com/images/M/MV5BNGZlNTFlOWMtMzUwNC00ZDdhLTk0MWUtOGZjYzFlOTBmNDdhXkEyXkFqcGdeQXVyMTUyNjIwMDEw._V1_.jpg",
+  url:"https://youtu.be/11OWuPcElJw",
+  movieType:"7",
+  showRating:"5"
+  },
+  {
+  id: "39",
+  name: "39Panchayat (TV Series 2020– ) -Panchayat (TV Series 2020– ) -Panchayat (TV Series 2020– ) -",
+  image: "https://m.media-amazon.com/images/M/MV5BMTllYzQ0MzctZTI4Ny00YTE2LTkzYjktMTc4ZWE2MDUxNzk2XkEyXkFqcGdeQXVyNzEyNTM4MTU@._V1_.jpg",
+  url:"https://youtu.be/ohmp6Q5xfXg",
+  movieType:"1234567",
+  showRating:"5"
+  
+  
+  },
+  {
+  id: "40",
+  name: "40Asur: Welcome to Your Dark Side (TV Series 2020– ) Asur: Welcome to Your Dark Side (TV Series 2020– ) ",
+  image: "https://m.media-amazon.com/images/M/MV5BNjczODVjMmMtNTVlNy00MjlkLWEyZjYtOThiYzMwZmIyNWZkXkEyXkFqcGdeQXVyMTY0NjI3Mjcx._V1_.jpg",
+  url:"https://youtu.be/11OWuPcElJw",
+  movieType:"741",
+  showRating:"5"
+  }
+  ];
+  
